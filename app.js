bb0bd59aa568bcd36deade5a991f8d2a7792515d
unknown <psabarikrishnan@gmail.com>
2023-08-20 15:40:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 8000;
+
+//express specific
+app.use('/static',express.static('static'))
+app.use(express.urlencoded())
+
+//pug specific
+app.set('view engine','pug')
+app.set('views',path.join(__dirname,'views'))
+
+//endpoints
+app.get('/',(req,res)=>{
+    
+    const params ={}
+    res.status(200).render('home.pug',params);
+})
+
+//start server
+app.listen(port,()=>{
+    console.log(`the application started successfuly on port ${port}`);
+})
