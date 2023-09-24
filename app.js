04376067ce2e6183b7ec634b82b2b33e6f48f2b7
unknown <psabarikrishnan@gmail.com>
2023-09-24 15:15:15 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser")
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactdance');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 const port = 8000;
+
+//define schema
+const contactSchema = new mongoose.Schema({
+  name:String,
+  phone:String,
+  email:String,
+  adress:String,
+  desc:String
+
+  });
+
+  const contact = mongoose.model('contact', contactSchema);
 
 //express specific
 app.use('/static',express.static('static'))
@@ -23,6 +45,16 @@ app.get('/contact',(req,res)=>{
     
     const params ={}
     res.status(200).render('contact.pug',params);
+})
+app.post('/contact',async (req,res)=>{
+    
+  const mydata = new contact(req.body);
+  await mydata.save().then(()=>{
+    res.send("this item has been saved to database")
+  }).catch(()=>{
+    res.status(400).send("item was not saved to the database")
+  })
+  res.status(200).render('contact.pug',params);
 })
 
 //start server
